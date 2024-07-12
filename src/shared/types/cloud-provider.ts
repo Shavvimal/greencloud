9c012ba6fd93a0dc42f7bd02fcca7a069be1847d
jack wiebe <122567698+jackwiebe0@users.noreply.github.com>
2024-07-12 09:13:31 -0700

    Update cloud-provider.ts

--- src/shared/types/cloud-provider.ts
+++ src/shared/types/cloud-provider.ts
@@ -2,4 +2,9 @@ export interface Region {
   name: string;
   code: string;
   cloudProvider: string;
+  lat: number;    // positive north, negative south
+  lon: number;    // positive east, negative west
+  sustainability: number;
+  latency: number;
+  fitScore: number;
 }
